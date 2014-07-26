a8ff694f1edf3d70543fed8822dfcae33fc66fd5
Alexandre Marcondes <alexandremarcondes@Alexandres-MacBook-Pro.local>
2014-07-26 15:35:38 -0300

    Initial Commit

---
+++ Liron/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Liron
+//
+//  Created by Alexandre Marcondes on 7/26/14.
+//  Copyright (c) 2014 Alexandre Marcondes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Liron/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Liron
+//
+//  Created by Alexandre Marcondes on 7/26/14.
+//  Copyright (c) 2014 Alexandre Marcondes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
